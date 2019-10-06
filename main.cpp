f869f67579aa01a7cfe27d5ea4a7f555c8ed580c
WolfNatural <42627045+wolfnatural@users.noreply.github.com>
2019-10-06 15:21:22 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float number1,number2;
+    char transaction;
+    
+    
+cout << "Hello My Friends ! I'am a Calculator ! My Creator : https://github.com/wolfnatural \n \n \n NOTE : Transactions: \n Addition : + \n Subtraction : - \n Multiplication : * \n Division : / \n \n" << endl;
+cout << "Please Enter The First Number : " << endl;
+cin >> number1; 
+cout << "Please Enter The Desired Transaction : " << endl;
+cin >> transaction;
+cout << "Please Enter The Second Number : " << endl;
+cin >> number2;
+
+if(transaction == '+'){
+    
+    cout << "Response : " << number1+number2 << endl;
+    
+}
+else if(transaction == '-'){
+    
+    cout << "Response : " << number1-number2 << endl;
+    
+}
+else if(transaction == '*'){
+    
+    cout << "Response : " << number1*number2 << endl;
+    
+}
+else if(transaction == '/'){
+    
+    cout << "Response : " << number1/number2 << endl;
+    
+}
+else{
+    
+    cout << "Something Went Wrong :( Pleas Try Again \n \n Contact : harunaydmrtr@outlook.com" << endl;
+    
+}
+    
+	
+	return 0;
+}
