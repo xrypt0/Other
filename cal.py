273730d054aa7fd43deeecec1a90033cc9ee844e
WolfNatural <42627045+wolfnatural@users.noreply.github.com>
2019-10-13 17:25:12 +0300

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,9 @@
+print("Hello My Friends ! I'm Factorial Calculator ! \n My Creator : https://github.com/wolfnatural ")
+
+x = int(input("Please Enter The Desired Number :"))
+y = 1
+for i in range(x):
+    y*= i+1
+
+print("Answer : ",y)
+print("Contact : harunaydmrtr@outlook.com OR https://github.com/wolfnatural ")
