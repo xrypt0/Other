f2e94085cd8d52da4573423e1d6dff2e75c8ff72
Crypto <42627045+xrypt0@users.noreply.github.com>
2020-11-16 11:09:39 +0300

    Update factoriel.py

--- factoriel.py
+++ factoriel.py
@@ -1,4 +1,4 @@
-print("Hello My Friends ! I'm Factorial Calculator ! \n My Creator : https://github.com/wolfnatural ")
+print("Hello My Friends ! I'm Factorial Calculator ! \n My Creator : https://github.com/xrypt0")
 
 x = int(input("Please Enter The Desired Number :"))
 y = 1
